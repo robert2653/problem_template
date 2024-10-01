8b6f797c6f41587a7a3bf7d418efcf542eeba92a
robert2653 <robert4812653@gmail.com>
2024-10-01 16:26:20 +0800

    Checker can run multiple .cpp

--- multiple_solution/MakeCase/checker.py
+++ multiple_solution/MakeCase/checker.py
@@ -5,15 +5,18 @@ import glob
 
 CHECKER_CPP_FILE = 'checker.cpp'
 CHECKER_EXECUTION_FILE = 'checker.out'
-USER_CPP_FILE = '../submissions/userCode.cpp'
-USER_EXECUTION_FILE = '../submissions/userCode.out'
 SAMPLE_FOLDER = '../data/sample/'
 SECRET_FOLDER = '../data/secret/'
 USER_FOLDER = '../data/user/'
 
-# 編譯
+USER_EXECUTION_FOLDER = '../executions/'
+MAMUAL_CPP_FILE = [
+    '../submissions/accepted/ac.cpp',
+    '../submissions/accepted/ac2.cpp'
+]
+
+# 編譯 checker
 subprocess.run(['g++', CHECKER_CPP_FILE, '-o', CHECKER_EXECUTION_FILE], check=True)
-subprocess.run(['g++', USER_CPP_FILE, '-o', USER_EXECUTION_FILE], check=True)
 
 def clear_data():
     pattern = re.compile(r'\.ans$')  # 匹配 .ans 結尾的檔案名稱
@@ -23,10 +26,10 @@ def clear_data():
                 file_path = os.path.join(folder, filename)
                 os.remove(file_path)
 
-def user_output(inputFile, userFile):
+def user_output(inputFile, userFile, user_executable):
     with open(inputFile, 'r') as f_in:
         stdin_content = f_in.read()
-        process = subprocess.run([USER_EXECUTION_FILE], input=stdin_content.encode(), capture_output=True)
+        process = subprocess.run([user_executable], input=stdin_content.encode(), capture_output=True)
     with open(userFile, 'w') as f_out:
         f_out.write(process.stdout.decode())
 
@@ -35,25 +38,45 @@ def checker_output(inputFile, ansFile, userFile, caseName):
     checker_process = subprocess.run(['./' + CHECKER_EXECUTION_FILE] + argv, capture_output=True, text=True)
     print('case', caseName, ':', checker_process.stderr, end='')
 
-if __name__ == '__main__':
-    clear_data()
+def testing(userCPP):
+    # 確定 user_executable 的路徑
+    user_name = os.path.splitext(os.path.basename(userCPP))[0]
+    user_executable = os.path.join(USER_EXECUTION_FOLDER, user_name + '.out')
+    # 編譯 userCPP
+    subprocess.run(['g++', userCPP, '-o', user_executable], check=True)
+    print(f'Testing {userCPP}.')
 
     # 處理 sample 測試案例
     sample_cases = glob.glob(os.path.join(SAMPLE_FOLDER, '*.in'))
     for input_file in sample_cases:
         case_name = os.path.splitext(os.path.basename(input_file))[0]
-        user_output(input_file, USER_FOLDER + case_name + '.ans')
+        user_output(input_file, USER_FOLDER + case_name + '.ans', user_executable)
         checker_output(input_file, SAMPLE_FOLDER + case_name + '.ans', USER_FOLDER + case_name + '.ans', case_name)
 
     # 處理 secret 測試案例
     secret_cases = glob.glob(os.path.join(SECRET_FOLDER, '*.in'))
     for input_file in secret_cases:
         case_name = os.path.splitext(os.path.basename(input_file))[0]
-        user_output(input_file, USER_FOLDER + case_name + '.ans')
+        user_output(input_file, USER_FOLDER + case_name + '.ans', user_executable)
         checker_output(input_file, SECRET_FOLDER + case_name + '.ans', USER_FOLDER + case_name + '.ans', case_name)
 
-    # 刪除執行檔
+    os.remove(user_executable)
+    print(f'Testing {userCPP} completed.\n')
+
+if __name__ == '__main__':
+    clear_data()
+
+    # 指定路徑中的執行檔案列表
+
+    # 遍歷 file_list
+    for userCPP in MAMUAL_CPP_FILE:
+        testing(userCPP)
+
+    # 遍歷 USER_EXECUTION_FOLDER, 找到 cpp
+    userCPPFiles = glob.glob(os.path.join(USER_EXECUTION_FOLDER, '*.cpp'))
+    for userCPP in userCPPFiles:
+        testing(userCPP)
+
+    # 刪除 checker 執行檔
     if os.path.exists(CHECKER_EXECUTION_FILE):
         os.remove(CHECKER_EXECUTION_FILE)
-    if os.path.exists(USER_EXECUTION_FILE):
-        os.remove(USER_EXECUTION_FILE)
